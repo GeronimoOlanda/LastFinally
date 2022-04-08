70ba5c7168389746f9759a78628395b0f3e51152
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-08 00:03:03 -0300

    created form and datas to register a user

--- src/views/Register/register.php
+++ src/views/Register/register.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./../../../public/css/register.css">
+    <link rel="shortcut icon" href="./../../../public/images/icon/enterprise.png" type="image/png">
     <title>Pagina Registro</title>
 </head>
 <body>
@@ -36,6 +37,7 @@
           </select><br>
 
           <button type="submit" class="btn btn-success">EFETUAR LOGIN</button>
+          <p> <span><a href="./../../../index.php" style="color:red;"><strong>Retornar Pagina de Login</strong></a> </span></p>
         </div>
       </div>
     </form>
