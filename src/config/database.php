1f8bdfbb8916118ddb32c0370570475c8865cc55
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-08 16:21:34 -0300

    Added config for log in database local

--- src/config/database.php
+++ src/config/database.php
@@ -1,1 +1,14 @@
+<?php
 
+$dbhost = 'localhost';
+$dbusername = 'root';
+$dbpassword = '';
+$dbname = 'LastFinally';
+
+try{
+  $link = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
+
+}catch(PDOException $exception){
+  echo("Ocoreu um problema, segue o erro: " . $exception);
+
+}
