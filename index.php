90d856d9e66babf7dba048e988cf5a2dba20f9d1
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-07 23:44:57 -0300

    creating css files and the code of index, register, created the header page too

--- index.php
+++ index.php
@@ -4,19 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/index.css">
     <title>LF - Pagina Inicial</title>
 </head>
 <body>
     <form action="./src/controllers/loginController.php" method="POST">
-        <div class="container">
-            <label for="username">Nome de Usuario ou seu email</label><br>
+        <div class="wrapper">
+          <div class="container">
+            <label class="label_form1" for="username">Nome de Usuario ou seu email</label><br>
             <input type="text" name="username" id="username" placeholder="Digite seu nome de usuario..."><br>
 
-            <label for="password">Senha</label><br>
-            <input type="password" name="userpassword" id="userpassword" placeholder="Digite sua senha..."><br>
-            <button type="submit">EFETUAR LOGIN</button>
-            <p>Não possui cadastro? <span><a href="./src/views/Register/register.php">Cliqu aqui</a> </span>e se cadastre</p>
+            <label class="label_form2" for="password">Senha</label><br>
+            <input type="password" name="userpassword" id="userpassword" placeholder="Digite sua senha..."><br><br>
+            <button type="submit" class="btn btn-success">EFETUAR LOGIN</button>
+            <p>Não possui cadastro? <span><a href="./src/views/Register/register.php">Clique aqui  </a> </span>e se cadastre</p>
+            </div>
         </div>
     </form>
+
+    <!-- SCRIPTS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- src/views/Register/register.php
+++ src/views/Register/register.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../../../public/css/register.css">
+    <title>Pagina Registro</title>
 </head>
 <body>
-    <h2>Tela de registro</h2>
+    <h2>DADOS CADASTRAIS</h2>
+    <form action="">
+      <div class="wrapper">
+        <div class="containr">
+          <label for="name">Nome Completo:</label><br>
+          <input type="text" name="name" id="name"><br><br>
+
+          <label class="label_form1"  for="email">Email:</label><br>
+          <input type="email" name="email" id="email"><br><br>
+
+          <label class="label_form2"  for="username">Usuario:</label><br>
+          <input type="text" name="username" id="username"><br><br>
+
+          <label class="label_form3" for="password">Senha:</label><br>
+          <input type="password" name="password" id="password"><br><br>
+
+          <label class="label_form4" for="repassword">Confirmar Senha:</label><br>
+          <input type="password" name="repassword" id="repassword"><br><br>
+
+          <select name="funcao">
+              <option value="1">Programação/TI</option>
+              <option value="2">Setor Contabil</option>
+              <option value="3">Setor RH</option>
+              <option value="4">Outro Setor</option>
+          </select><br>
+
+          <button type="submit" class="btn btn-success">EFETUAR LOGIN</button>
+        </div>
+      </div>
+    </form>
 </body>
 </html>
