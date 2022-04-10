e5b49075e3659a6f35907b14904d2ced1d1370b2
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-10 19:03:46 -0300

    renomeado a posta de metas e modificando onde el esta inserido

--- src/views/Register/register.php
+++ src/views/Register/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include("./../adicionais/metas.php"); ?>
+    <?php include("./../Metas/metas.php"); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./../../../public/css/register.css">
     <link rel="shortcut icon" href="./../../../public/images/icon/enterprise.png" type="image/png">
