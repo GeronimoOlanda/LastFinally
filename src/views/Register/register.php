651831daab5860b8ffaa04cc5ca5d776ead80706
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-10 13:02:49 -0300

    adicionando meta tags para redes sociai

--- src/views/Register/register.php
+++ src/views/Register/register.php
@@ -1,14 +1,14 @@
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("./../adicionais/metas.php"); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./../../../public/css/register.css">
     <link rel="shortcut icon" href="./../../../public/images/icon/enterprise.png" type="image/png">
-    <title>Pagina Registro</title>
+    <title>LF - Pagina Registro</title>
 </head>
 <body>
     <h2>DADOS CADASTRAIS</h2>
@@ -41,7 +41,7 @@
 
           <button type="submit" class="btn btn-success">EFETUAR LOGIN</button>
           <p> <span><a href="./../../../index.php" style="color:red;"><strong>Retornar Pagina de Login</strong></a> </span></p>
-        
+
         </div>
       </div>
     </form>
---
+++ src/views/adicionais/metas.php
@@ -0,0 +1,12 @@
+  <meta property="fb:app_id" content="your_app_id" /> <!-- Para ferramntas de analises -->
+  <meta name="twitter:site" content="@website-username"><!-- Para ferramentas de analises -->
+  <!-- META SOCIAL GERAL-->
+  <meta property="og:title" content="The Rock" />
+  <meta property="og:type" content="video.movie" />
+  <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+  <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+  <!-- META TWITTER -->
+  <meta name="twitter:title" content="European Travel Destinations ">
+  <meta name="twitter:description" content=" Offering tour packages for individuals or groups.">
+  <meta name="twitter:image" content=" http://euro-travel-example.com/thumbnail.jpg">
+  <meta name="twitter:card" content="summary_large_image">
