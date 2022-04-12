01c5bec27331ff5b680e6330a0636b711eb8c04b
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-04-11 22:56:13 -0300

    added controler to register and verify if exist an empty value

--- src/controllers/registerController.php
+++ src/controllers/registerController.php
@@ -7,37 +7,42 @@ $email = $_POST["email"];
 $username = $_POST["username"];
 $password = md5($_POST["password"]);
 $repassword = md5($_POST["repassword"]);
-$selected = $_POST["selected"];
 
-//validation datas
-
-//insert
-try{
-  $sql = 'INSERT INTO users (fullname, email, username, password, codigo_departamento) VALUES (:fullname, :email, :username, :password, :selected)';
-
-  $statement = $link->prepare($sql);
-  $statement->bindParam(":fullname", $fullname, PDO::PARAM_STR);
-  $statement->bindParam(":email",    $email,    PDO::PARAM_STR);
-  $statement->bindParam(":username", $username, PDO::PARAM_STR);
-  $statement->bindParam(":password", $password, PDO::PARAM_STR);
-  $statement->bindParam(":selected", $selected, PDO::PARAM_STR);
-  $statement->execute();
-  $result = $statement-> fetchAll(PDO::FETCH_OBJ);
-
-  header("location: ../views/Register/register.php");
-
-  $message = $cadastro_message_success;
-
-  if($statement->rowCount() > 0){
-    echo $message;
-  }
-
-
-}catch(PDOException $exception){
-  echo("Ocorreu um erro" . $exception);
-
+if(isset($_POST["selected"])){
+  $selected = $_POST["selected"];
 }
 
+//validation datas
+if(empty($fullname) || empty($email)|| empty($username)|| empty($password)|| empty($repassword)|| empty($selected)){
+    $message = "verifique os campos vazios e preencha-os";
+}else{
+  //insert
+  try{
+    $sql = 'INSERT INTO users (fullname, email, username, password, codigo_departamento) VALUES (:fullname, :email, :username, :password, :selected)';
+
+    $statement = $link->prepare($sql);
+    $statement->bindParam(":fullname", $fullname, PDO::PARAM_STR);
+    $statement->bindParam(":email",    $email,    PDO::PARAM_STR);
+    $statement->bindParam(":username", $username, PDO::PARAM_STR);
+    $statement->bindParam(":password", $password, PDO::PARAM_STR);
+    $statement->bindParam(":selected", $selected, PDO::PARAM_STR);
+    $statement->execute();
+    $result = $statement-> fetchAll(PDO::FETCH_OBJ);
+    $message = "cadastro feito com sucesso!";
+    header("location: ../views/Register/register.php");
+
+
+
+    if($statement->rowCount() > 0){
+      echo $message;
+    }
+
+
+  }catch(PDOException $exception){
+    echo("Ocorreu um erro" . $exception);
+
+  }
+}
 
 //update
 
